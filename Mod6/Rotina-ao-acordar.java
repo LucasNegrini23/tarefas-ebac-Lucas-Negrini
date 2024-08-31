8810b587c9a4479c79508879384a22efee42782d
Lucas Negrini <lucasmnegrini25@gmail.com>
2024-08-31 20:37:05 +0100

    Tarefa modulo 6 - ROTINA AO ACORDAR

---
+++ Mod6/Rotina-ao-acordar.java
@@ -0,0 +1,27 @@
+algoritmo "Rotina_Ao_Acordar"
+
+inicio
+    Passo 1: Despertar ao som do despertador;
+    
+    Passo 2: Desligar o alarme;
+    
+    Passo 3: Após delisgar alarme, Espreguiçar-se;
+    
+    Passo 4: Após espreguiçar-se, Levantar da cama;
+    
+    Passo 5: Após levantar da cama ir ao banheiro;
+
+    Passo 6: Após entrar no banheiro, escovar os dentes;
+
+    Passo 7: Após escovar os dentes, lavar o rosto;
+
+    Passo 8: Aopós lavar o rosto, Vestir-se;
+    
+    Passo 9: Após vestir=se ir para cozinha,
+
+    Passo 10: Após entrar na cozinha, Tomar café da manhã;
+    
+    Passo 11: Após tomar o café da manhã, Conferir agenda do dia;
+  
+    Passo 12: Aopós conferir a agenda, Sair de casa.
+fim
