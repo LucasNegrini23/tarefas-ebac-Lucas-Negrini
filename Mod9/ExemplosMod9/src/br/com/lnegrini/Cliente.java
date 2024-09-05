03fc8d57aa93a9aac3199c3e4eb28cf47d263e9c
Lucas Negrini <lucasmnegrini25@gmail.com>
2024-09-05 15:11:29 +0100

    Mod9

---
+++ Mod9/ExemplosMod9/src/br/com/lnegrini/Cliente.java
@@ -0,0 +1,28 @@
+package br.com.lnegrini;
+
+/**
+ * @author lucas.negrini
+ */
+
+public class Cliente {
+
+//Wrappers tem metodos a mais do que os Primitivos ex: Integer > int)
+
+        private Integer codigo;
+
+        private Long codigoMaior = 1204050l;
+
+        private Double valorDecimal = 10.1;
+
+        private Boolean status = false;
+
+        private Character letra = 'A';
+
+        public void main(String[] args) {
+            this.codigo.byteValue();
+        }
+        public void teste() {
+            this.codigo = null;
+        }
+}
+
