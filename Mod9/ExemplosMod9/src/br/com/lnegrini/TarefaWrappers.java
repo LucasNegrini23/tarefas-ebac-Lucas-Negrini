081fc308b11c8f022afe437c6a7f2f9d94ebded4
Lucas Negrini <lucasmnegrini25@gmail.com>
2024-09-05 16:02:34 +0100

    Mod9

--- Mod9/ExemplosMod9/src/br/com/lnegrini/TarefaWrappers.java
+++ Mod9/ExemplosMod9/src/br/com/lnegrini/TarefaWrappers.java
@@ -8,10 +8,12 @@ public class TarefaWrappers {
 
     public static void main(String[] args) {
 
-        long cpf = 111115l;
+        long cpf = 111115L;  // Primitivo 'long'
         System.out.println("CPF: " + cpf);
-        Long cpf1 = Long.valueOf(111116L);
+
+        Long cpf1 = Long.valueOf(cpf);  // Convertendo 'cpf' (long) para 'Long'
         System.out.println("CPF1: " + cpf1);
+
 
     }
 }
